425fc1f88f2a2b83105b1ad141635d7ebae8c3e4
Tachibana Shin <thanhnguyennguyen1995@gmail.com>
2021-12-22 14:35:46 +0700

    first commit

---
+++ meta.js
@@ -0,0 +1,93 @@
+const pkg = require("./package.json");
+
+const templateVersion = pkg.version;
+
+module.exports = {
+  metalsmith: {},
+  helpers: {
+    if_or(v1, v2, options) {
+      if (v1 || v2) {
+        return options.fn(this);
+      }
+
+      return options.inverse(this);
+    },
+    exists(value, options) {
+      if (value) {
+        return options.fn(this);
+      }
+
+      return options.inverse(this);
+    },
+    if_xor(v1, v2, v3, options) {
+      if (v1 || v2 === v3) {
+        return options.fn(this);
+      }
+
+      return options.inverse(this);
+    },
+    if_ne(v1, v2, options) {
+      if (v1 !== v2) {
+        return options.fn(this);
+      }
+
+      return options.inverse(this);
+    },
+    if_in(v1, v2, options) {
+      if (v2 in v1) {
+        return options.fn(this);
+      }
+
+      return options.inverse(this);
+    },
+    template_version() {
+      return templateVersion;
+    },
+  },
+
+  prompts: {
+    name: {
+      type: "string",
+      required: true,
+      message: "Project name",
+    },
+    description: {
+      type: "string",
+      required: false,
+      message: "Project description",
+      default: "A package project",
+    },
+    author: {
+      type: "string",
+      message: "Author",
+    },
+    ts: {
+      type: "confirm",
+      message: "Use TypeScript?",
+      default: true,
+    },
+    mode: {
+      type: "checkbox",
+      message: "Select mode for package:",
+      choices: [
+        {
+          name: "Browser",
+          value: "browser",
+            },
+        {
+          name: "Node.js",
+          value: "node",
+            },
+        {
+          name: "Both",
+          value: "both",
+            },
+          ],
+    },
+  },
+  filters: {
+    "tsconfig.json": "ts",
+    "**/*.js": "ts === false",
+    "**/*.ts": "ts",
+  },
+};
---
+++ template/rollup.config.ts
@@ -0,0 +1,168 @@
+import commonjs from "rollup-plugin-commonjs";
+{{#ts}}
+import dts from "rollup-plugin-dts";
+{{/ts}}
+import esbuild from "rollup-plugin-esbuild";
+import resolve from "rollup-plugin-node-resolve";
+// import { terser } from "rollup-plugin-terser";
+
+import { name, version } from "./package.json";
+
+const banner = `/**
+ * This library created by {{ author }} (c) ${new Date().getFullYear()}
+ * MIT License
+ * ${name} version ${version}
+ * */`;
+
+const options = [
+  {
+    input: "src/index.ts",
+    plugins: [
+      esbuild({
+        {{#ts}}
+        // All options are optional
+        include: /\.ts$/,
+        {{/ts}}
+        {{#if_not ts}}
+        incude: /\.c?js$/,
+        {{/if_not}}
+        exclude: /node_modules/, // default
+        sourceMap: false, // default
+        minify: false,
+        target: "es2017", // default, or 'es20XX', 'esnext'
+        jsx: "transform", // default, or 'preserve'
+        // Like @rollup/plugin-replace
+        define: {
+          __VERSION__: `"${version}"`,
+        },
+        {{#ts}}
+        tsconfig: "./tsconfig.json", // default
+        {{/ts}}
+        // Add extra loaders
+        loaders: {
+          // Add .json files support
+          // require @rollup/plugin-commonjs
+          ".json": "json",
+        },
+      }),
+      resolve({
+        browser: true,
+        preferBuiltins: false,
+      }),
+      commonjs(),
+    ],
+    output: [
+      {{#if_or mode "browser" "both"}}
+      {
+        file: `dist/${name}.umd.js`,
+        format: "umd",
+        plugins: [],
+        name: `${name}`,
+        banner,
+        strict: true,
+      },
+      {{/if_or}}
+      {{#if_or mode "node" "both"}}
+      {
+        file: `dist/${name}.cjs.js`,
+        format: "cjs",
+        plugins: [],
+        name: `${name}`,
+        banner,
+        strict: true,
+      },
+      {{/if_or}}
+      // {
+      //   file: `dist/${name}.umd.min.js`,
+      //   format: "umd",
+      //   plugins: [terser()],
+      //   name: `${name}`,
+      // },
+      {
+        file: `dist/${name}.esm.js`,
+        format: "esm",
+        plugins: [],
+        exports: "auto",
+        banner,
+        strict: true,
+      },
+      // {
+      //   file: `dist/${name}.esm.min.js`,
+      //   format: "esm",
+      //   plugins: [terser()],
+      //   exports: "auto",
+      // },
+    ],
+  },
+  {
+    input: "src/index.ts",
+    plugins: [
+      esbuild({
+        {{#ts}}
+        // All options are optional
+        include: /\.ts$/,
+        {{/ts}}
+        {{#if_not ts}}
+        incude: /\.c?js$/,
+        {{/if_not}}
+        exclude: /node_modules/, // default
+        sourceMap: false, // default
+        minify: true,
+        target: "es2017", // default, or 'es20XX', 'esnext'
+        jsx: "transform", // default, or 'preserve'
+        // Like @rollup/plugin-replace
+        define: {
+          __VERSION__: `"${version}"`,
+        },
+        {{#ts}}
+        tsconfig: "./tsconfig.json", // default
+        {{/ts}}
+        // Add extra loaders
+        loaders: {
+          // Add .json files support
+          // require @rollup/plugin-commonjs
+          ".json": "json",
+        },
+      }),
+      resolve({
+        browser: true,
+        preferBuiltins: false,
+      }),
+      commonjs(),
+    ],
+    output: [
+      {{#if_or mode "browser" "both"}}
+      {
+        file: `dist/${name}.umd.min.js`,
+        format: "umd",
+        plugins: [],
+        name: `${name}`,
+        banner,
+        strict: true,
+      },
+      {{/if_or}}
+      {
+        file: `dist/${name}.esm.min.js`,
+        format: "esm",
+        plugins: [],
+        exports: "auto",
+        banner,
+        strict: true,
+      },
+    ],
+  },
+  {{#ts}}
+  {
+    input: "src/index.ts",
+    output: [
+      {
+        file: `dist/${name}.d.ts`,
+        format: "es",
+      },
+    ],
+    plugins: [dts()],
+  },
+  {{/ts}}
+];
+
+export default options;
